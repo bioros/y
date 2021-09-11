f3c6447197afb0e0340ad7ee8568343a8ed4c82f
bioros <90413773+bioros@users.noreply.github.com>
2021-09-11 05:50:44 +0300

    Update hm.sh

--- hm.sh
+++ hm.sh
@@ -1,6 +1,6 @@
 sudo -i
+mkdir hm && cd hm
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar -zxvf hellminer*.tar.gz
-cd hellminer_cpu_linux
 chmod +x mine.sh && nano mine.sh
 ./mine.sh
