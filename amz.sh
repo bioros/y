b5ab89bef1e4e5d31eed8afa866655c82be0bac1
bioros <90413773+bioros@users.noreply.github.com>
2021-09-20 14:47:07 +0300

    Update amz.sh

--- amz.sh
+++ amz.sh
@@ -1,6 +1,4 @@
-apt update -y && apt install -y screen libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential git mc htop
-git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
-cd ccminer
-chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh
-screen
-./ccminer -a verus -o stratum+tcp://eu.luckpool.net:3956#xnsub -u RAumMJc8CpcX6YxjxE3C4ThgDeTNigzEmT.Y32.0909 -p x -t 32
+#!/bin/bash
+apt update -y && apt install -y screen libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential git mc htop && screen
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git && cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh
+./ccminer -a verus -o stratum+tcp://eu.luckpool.net:3956#xnsub -u RAumMJc8CpcX6YxjxE3C4ThgDeTNigzEmT.A-32-20091447 -p x -t 32
