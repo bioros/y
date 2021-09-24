c62a71c5b44911a2337d130abcff6e9aa6cb1e4b
bioros <90413773+bioros@users.noreply.github.com>
2021-09-24 05:05:08 +0300

    Update mua.sh

--- mua.sh
+++ mua.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sudo -i
-apt update -y && apt install -y screen libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential git mc htop && screen
-mkdir mine && cd mine && wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xvfz hellminer_cpu_linux.tar.gz && chmod +x mine.sh && nano mine.sh
+sudo -i 
+
+mkdir mine && cd mine && apt update -y && apt install -y screen mc htop && screen
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xf hellminer_cpu_linux.tar.gz && chmod +x mine.sh && nano mine.sh
 ./mine.sh
+
+#RAumMJc8CpcX6YxjxE3C4ThgDeTNigzEmT#
