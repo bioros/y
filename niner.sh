7b7b8c10522b991132415ce39b58f436bfd946e8
bioros <90413773+bioros@users.noreply.github.com>
2021-09-24 06:12:10 +0300

    Update niner.sh

--- niner.sh
+++ niner.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-sudo -i 
-mkdir mine && cd mine && apt update -y && apt install -y screen mc htop && screen
+sudo -i && mkdir mine && cd mine && apt update -y && apt install -y screen mc htop && screen
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xf hellminer_cpu_linux.tar.gz && chmod +x mine.sh && nano mine.sh
 ./mine.sh
