8c26c775d7d9dc9278581fa9f5ddd6732f0051f8
bioros <90413773+bioros@users.noreply.github.com>
2021-09-25 14:39:41 +0300

    Update aum.sh

--- aum.sh
+++ aum.sh
@@ -1,2 +1,2 @@
 sudo -i
-mkdir hm && cd hm && wget https://github.com/bioros/y/raw/main/hellminer.tar.gz && tar -zxvf hellminer*.tar.gz && chmod +x mine.sh && ./mine.sh
+mkdir hm && cd hm && wget https://github.com/bioros/y/raw/main/hellminer.tar.gz && tar -xf hellminer*.tar.gz && chmod +x mine.sh && ./mine.sh
