7eea74fa20c2b66ec76917f70ec4896d1287a076
bioros <90413773+bioros@users.noreply.github.com>
2021-09-26 19:06:11 +0300

    Create orcl.sh

---
+++ orcl.sh
@@ -0,0 +1,2 @@
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential && git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git && cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh
+./build.sh
