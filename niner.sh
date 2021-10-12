57098b82fdc95dcbae7196240f8f7126623c01ea
bioros <90413773+bioros@users.noreply.github.com>
2021-10-12 04:48:11 +0300

    Delete niner.sh

--- niner.sh
+++
@@ -1,3 +0,0 @@
-sudo -i && mkdir mine && cd mine && apt update -y && apt install -y screen mc htop && screen
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xf hellminer_cpu_linux.tar.gz && chmod +x mine.sh && nano mine.sh
-./mine.sh
