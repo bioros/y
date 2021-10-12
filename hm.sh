35627c2d48bf59b10d4c8fdc7ddccaa5db3d3a10
bioros <90413773+bioros@users.noreply.github.com>
2021-10-12 04:49:22 +0300

    Delete hm.sh

--- hm.sh
+++
@@ -1,6 +0,0 @@
-sudo -i
-mkdir hm && cd hm
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar -zxvf hellminer*.tar.gz
-chmod +x mine.sh && nano mine.sh
-./mine.sh
