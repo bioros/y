27a0087dc40a07c258d342a85a23abe12f5e3d48
bioros <90413773+bioros@users.noreply.github.com>
2021-10-13 16:10:39 +0300

    Delete orcl.sh

--- orcl.sh
+++
@@ -1,2 +0,0 @@
-sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential && git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git && cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh
-./build.sh
